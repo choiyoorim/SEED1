968351f7f9e661da3036d3a9c1ff3eb939462b85
YNKa526 <rkdbsk0526@gmail.com>
2021-07-26 15:28:53 +0900

    21.07.26 mypage :yuna

--- src/Pages/Mypage/Mypage.js
+++ src/Pages/Mypage/Mypage.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FiSettings } from "react-icons/fi";
 import { IconContext } from 'react-icons';
+import './../../Components/color.css'
 import  '../Mypage/Mypage.css';
 import user from '../../Components/Movie_poster/movie1.png';
-import Navi from '../../Components/Navi';
 
+var db = require
 class Mypage extends Component {
     render(){
        return (
@@ -20,22 +21,27 @@ class Mypage extends Component {
                         <FiSettings color= 'var(--seed-yelow)' size ='25px'/>
                     </Link>
                 </div>
-                <div class="user_write">
-                    <h3>내 Seeds</h3>
-                    <div className="write_info">
-                        <span id="movie">영화</span><span id="title">글 제목</span><span id="num">조회수</span><span id="date">작성일</span>
+
+                <div className="popularSeeds">
+                <h3>인기 글</h3>
+                    <div className="PSeedsContainer">
+                        <div id="PSElement"></div>
+                        <div id="PSElement"></div>
+                        <div id="PSElement"></div>
+                        <div id="PSElement"></div>
+                        <div id="PSElement"></div>
                     </div>
-                    <form id="written">
+                </div>
+
+                <div class="user_write">
+                <h3>Seeds</h3>
+                    <div className="write_info">
+                        <div className="myseeds">
+                            <span id="Wmovie">영화</span><span id="Wtitle">글 제목</span><span id="Wnum">조회수</span><span id="Wdate">작성일</span>
+                        </div>
+                        <form id="written">
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li><span id="Wmovie">영화 이름</span><span id="Wtitle"> 제목1</span><span id="Wnum">3</span><span id="Wdate">2021.07.23</span></li>
                             <li></li>
                             <li></li>
                             <li></li>
@@ -43,6 +49,8 @@ class Mypage extends Component {
                             <li></li>
                         </ul>
                     </form>
+                    </div>
+                    
                 </div>
             </section>
        );
