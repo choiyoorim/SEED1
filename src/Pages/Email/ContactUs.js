66e8a209d0b16764f0998abae1586c9d978c6867
YNKa526 <rkdbsk0526@gmail.com>
2021-08-18 19:31:22 +0900

    ContactUs, Navi - yuna

--- src/Pages/Email/ContactUs.js
+++ src/Pages/Email/ContactUs.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
 import './ContactUs.css';
 
 export default function ContactUs() {
 
+
+  const userNickname= localStorage.getItem('userNickname');
+  const userEmail = localStorage.getItem('userEmail');
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
   function sendEmail(e) {
-    e.preventDefault();
+    e.preventDefault(); //제출 시 내용 사라지지 않게 함
+
+    if(title.length === 0){
+      return alert("제목을 입력하세요");
+    }
+
+    if(contents.length == 0){
+      return alert("내용을 입력하세요");
+    }
 
     emailjs.sendForm('service_j3hy8f7', 'template_f08s5je', e.target, 'user_zFINoRCdzAAr5RPP6Rk3H')
       .then((result) => {
@@ -15,8 +29,11 @@ export default function ContactUs() {
           console.log(error.text);
       });
     e.target.reset();
+    setTitle("");
+    setContents("");
     alert('의견내주셔서 감사합니다. 메시지가 전송되었습니다.')
-  }
+
+  };
 
   return (
     <section>
@@ -25,15 +42,15 @@ export default function ContactUs() {
           사이트에 대한 의견, 불편사항 등을 보내주세요. seed가 발전하는데 큰 도움이 될 것입니다.
         </p>
         <form className="contact-form" onSubmit={sendEmail}>
-          <input type="hidden" name="contact_number" />
+          <input type="hidden" name="contact_number"/>
           <label>Name</label>
-          <input type="text" name="user_name" />
+          <input type="text" name="user_name" value={userNickname} readOnly/>
           <label>Email</label>
-          <input type="email" name="user_email" />
+          <input type="text" name="user_email" value={userEmail} readOnly/>
           <label>Title</label>
-          <input type="text" name="title" />
+          <input type="text" name="title" onChange={(e)=>{setTitle(e.target.value);}}/>
           <label>Message</label>
-          <textarea name="message" />
+          <textarea name="message" onChange={(e)=>{setContents(e.target.value);}}/>
           <input type="submit" value="Send" />
         </form>
       </div>      
