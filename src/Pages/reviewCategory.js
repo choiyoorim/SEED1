e906dc2aa1120b3ff18a7de714709c1b7c731d96
YNKa526 <rkdbsk0526@gmail.com>
2021-08-29 15:29:24 +0900

    reviewCategory(yuna)

---
+++ src/Pages/reviewCategory.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Category = [
+  {
+    title: '줄 글',
+    path: '/',
+    cName: 'category-text'
+  },
+  {
+    title: '카드 뉴스',
+    path: '/',
+    cName: 'category-text'
+  },
+  {
+    title: '영상',
+    path: '/',
+    cName: 'category-text'
+  }
+];
