0b711be654033adfec5f1943047cf64a19847bd5
YNKa526 <rkdbsk0526@gmail.com>
2021-10-13 17:10:34 +0900

    add subscribe button click design

--- src/Pages/See/ReviewDetail.js
+++ src/Pages/See/ReviewDetail.js
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 import {FaHeart} from "react-icons/fa";
 import {FiHeart} from "react-icons/fi";
 import {FaEye} from "react-icons/fa";
+import {FiCheckSquare} from "react-icons/fi";
 import {BsFillBackspaceReverseFill, BsFillPersonFill} from "react-icons/bs";
 
 
@@ -14,6 +15,7 @@ class ReviewDetail extends Component {
     review: [],
     like: false,
     isLike: false,
+    isSubscribe: false,
   };
 
   //reviewID로 리뷰 가져오기
@@ -42,12 +44,22 @@ class ReviewDetail extends Component {
     this.setState({
       isLike: true,
     });
-}
+  }
+  toggleSubscribe = () => {
+    this.state.isSubscribe ?
+    this.setState({
+      isSubscribe: false,
+    })
+    :
+    this.setState({
+      isSubscribe: true,
+    });
+  }
 
 
   render () {
     const {location} = this.props;
-    const { isLike } = this.state;
+    const { isLike, isSubscribe } = this.state;
     return (
       <div className="ReviewDetail_container">
         <div className="review_content">
@@ -56,13 +68,19 @@ class ReviewDetail extends Component {
           <div className="review_text_box">
             <p className="review_text">{location.state.content}</p>
           </div>
-          <div>
-            <p><BsFillPersonFill/>  {location.state.writer}</p>
-            <div>
+          <div className="subLikeBut_container">
+            <div className="userSub_box">
+              <p><BsFillPersonFill/>  {location.state.writer}</p>
+              <button className="sub_but" onClick={()=>this.toggleSubscribe()}>
+                {isSubscribe ? <FiCheckSquare style={{ color: '#ffbb00', fontSize: '20px'}}/>:<FiCheckSquare style={{fontSize: '20px'}}/>}  
+              </button>
+            </div>
+            
+            <div className="Like_box">
               <button className="like_but" onClick={()=>this.toggleLike()}>
                 {isLike?<FaHeart style={{ color: '#ffbb00', fontSize: '20px'}}/>:<FiHeart style={{fontSize: '20px'}}/>}  
               </button>
-              {location.state.likeC}
+              <p>{location.state.likeC}</p>
             </div>
             
             {/* <p><FaEye/>  {location.state.viewC}</p> */}
