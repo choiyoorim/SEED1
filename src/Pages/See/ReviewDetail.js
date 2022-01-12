3b38573830b761a3c9337d0c415a0951049e497b
jeayoungKim529 <jeay529@gmail.com>
2022-01-12 19:29:58 +0900

    [FIX] nav error

--- src/Pages/See/ReviewDetail.js
+++ src/Pages/See/ReviewDetail.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
 import './ReviewDetail.css';
 import axios from 'axios';
-import Button from "../../Components/Button"
-import {Link} from "react-router-dom";
-import {FaHeart} from "react-icons/fa";
-import {FiHeart} from "react-icons/fi";
+import Button from "../../Components/Button";
 import {FaEye} from "react-icons/fa";
 import {FiCheckSquare} from "react-icons/fi";
 import {BsFillBackspaceReverseFill, BsFillPersonFill} from "react-icons/bs";
 import Subscribe from '../../Components/views/Sections/Subscribe';
+import Like from '../../Components/views/Sections/Like';
 import Modification from '../../Components/views/Sections/Modification';
 
 const userID = localStorage.getItem('userID');
 
 class ReviewDetail extends Component {
   state = {
-    isLoading: true,
-    review: [],
-    like: false
+    review: []
   };
 
+  //리뷰E 가져오기
   getReview = async () => {
-    const movieCODE = this.props.match.params.id;
-    await axios.post('//localhost:3002/api/see/movie/review', {movieCODE})
+    const reviewID = this.props.match.params.id;
+    axios.post('//localhost:3002/api/see/review/reviewE/info', {reviewID})
     .then((res)=>{
-        this.setState({ review : res.data, isLoading: false });
+        this.setState({ review : res.data[0] });
     });
   };
 
-  
   componentDidMount(){
     const { location, history } = this.props;
     if(location.state === undefined) {
@@ -56,40 +52,42 @@ class ReviewDetail extends Component {
   }
 
   render () {
-    const {location} = this.props;
-    //writerID 로컬 저장소에 저장
-    //seeReviewID는 ReviewDetail페이지에서 보고있는 리뷰 ID
-    localStorage.setItem('writerID', location.state.writer);
-    localStorage.setItem('seeReviewID', location.state.id);
+    const { review } = this.state;
+    // //writerID 로컬 저장소에 저장
+    // //seeReviewID는 ReviewDetail페이지에서 보고있는 리뷰 ID
+    // localStorage.setItem('writerID', location.writer);
+    // localStorage.setItem('seeReviewID', review.id);
 
     return (
       <div className="ReviewDetail_container">
         <div className="review_content">
-          <h3 className="review_title">{location.state.title}</h3>
+          <h3 className="review_title">{review.reviewTitle}</h3>
           <hr className="line"></hr>
           <div className="subLikeBut_container">
             <span className="userSub_box">
-              <p><BsFillPersonFill/>  {this.state.writer = location.state.writer}</p>
+              <p><BsFillPersonFill/>  {review.userID}</p>
             </span>
             
             <Subscribe/>
 
-            {location.state.title === undefined ? <></> : <Modification/>}
+            <div style={{display: (userID === review.userID) ? 'none' : 'block'}}>
+              <Like reviewID = {review.reviewID}/>
+            </div>
 
-            {(location.state.writer === userID && location.state.title === undefined) ? 
-            <div className="delete-button" onClick={()=>this.deleteShortReview(location.state.id)}>
+            {review.title === undefined ? <></> : <Modification/>}
+
+            {(review.writer === userID && review.title === undefined) ? 
+            <div className="delete-button" onClick={()=>this.deleteShortReview(review.id)}>
             <Button size="sm" type="delete">삭제</Button>
           </div>
           : <></>}
             
 
-            {/* 좋아요버튼 */}
-
             {/* 조회수 */}
             {/* <p><FaEye/>  {location.state.viewC}</p> */}
           </div>
           <div className="review_text_box">
-            <p className="review_text">{location.state.content}</p>
+            <p className="review_text">{review.reviewContent}</p>
           </div>
           
         </div>
